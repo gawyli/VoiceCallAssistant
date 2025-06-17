1c05628b6a583ac672760e567ade8d625c079af4
gawyli <mystudypw@gmail.com>
2025-06-17 08:37:05 +0100

    feat: extracted realtime ai to service

--- VoiceCallAssistant/Interfaces/IRealtimeAiService.cs
+++ VoiceCallAssistant/Interfaces/IRealtimeAiService.cs
@@ -1,6 +1,8 @@
-﻿namespace VoiceCallAssistant.Interfaces;
+﻿using OpenAI.RealtimeConversation;
+
+namespace VoiceCallAssistant.Interfaces;
 
 public interface IRealtimeAiService
 {
-    
+    Task<RealtimeConversationSession> CreateConversationSessionAsync(CancellationTokenSource cts, string? systemMessage = null, ConversationSessionOptions? conversationSessionOptions = null);
 }

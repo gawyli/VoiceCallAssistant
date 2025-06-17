59f37931ff034c7892fb2e8d3c97856626a5d233
gawyli <mystudypw@gmail.com>
2025-06-17 19:58:03 +0100

    feat: extract two metods from media to twilio service

--- VoiceCallAssistant/Interfaces/ITwilioService.cs
+++ VoiceCallAssistant/Interfaces/ITwilioService.cs
@@ -1,8 +1,24 @@
-﻿namespace VoiceCallAssistant.Interfaces;
+﻿using OpenAI.RealtimeConversation;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+
+namespace VoiceCallAssistant.Interfaces;
 
 public interface ITwilioService
 {
     public void CreateClient();
     public string MakeCall(string toPhoneNumber);
     public string ConnectWebhook();
+
+    public Task ReceiveFrom(
+            WebSocket webSocket,
+            CancellationToken ct,
+            Action<string> setStreamSid,
+            Action<BinaryData, long> handleAudio,
+            ConcurrentQueue<string> markQueue);
+
+    public Task SendTo(RealtimeConversationSession session,
+        CancellationToken ct,
+        Func<ConversationItemStreamingPartDeltaUpdate, Task> handleAudioDelta,
+        Func<ConversationInputSpeechStartedUpdate, Task> handleSpeechStarted);
 }
---
+++ VoiceCallAssistant/Messages/BaseEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace VoiceCallAssistant.Messages;
+
+public abstract class BaseEvent
+{
+    public string @Event { get; set; } = null!;
+}

81f6c250e7c1326d4a5b072462e3da26532c263a
Pawel Wisniewski <68349776+gawyli@users.noreply.github.com>
2025-06-18 10:10:44 +0000

    feat: set call request from routne.PhoneNumber

--- VoiceCallAssistant/Controllers/RoutineController.cs
+++ VoiceCallAssistant/Controllers/RoutineController.cs
@@ -1,44 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using VoiceCallAssistant.Interfaces;
-using VoiceCallAssistant.Models;
+﻿// using Microsoft.AspNetCore.Mvc;
+// using VoiceCallAssistant.Interfaces;
+// using VoiceCallAssistant.Models;
 
-namespace VoiceCallAssistant.Controllers;
+// namespace VoiceCallAssistant.Controllers;
 
-[ApiController]
-[Route("api/routine")]
-public class RoutineController : ControllerBase
-{
-    private readonly IRepository _repository;
+// [ApiController]
+// [Route("api/routine")]
+// public class RoutineController : ControllerBase
+// {
+//     private readonly IRepository _repository;
 
-    public RoutineController(IRepository repository)
-    {
-        _repository = repository;
-    }
+//     public RoutineController(IRepository repository)
+//     {
+//         _repository = repository;
+//     }
 
-    [HttpPost("create", Name = "CreateRoutine")]
-    public async Task<IActionResult> CreateRoutinePost(CancellationToken cancellationToken)
-    {
-        var routine = CreateRoutine();
-        await _repository.AddAsync(routine, cancellationToken);
+//     [HttpPost("create", Name = "CreateRoutine")]
+//     public async Task<IActionResult> CreateRoutinePost(CancellationToken cancellationToken)
+//     {
+//         var routine = CreateRoutine();
+//         await _repository.AddAsync(routine, cancellationToken);
 
-        return CreatedAtAction(nameof(CreateRoutinePost), new { id = routine.Id }, routine);
-    }
+//         return CreatedAtAction(nameof(CreateRoutinePost), new { id = routine.Id }, routine);
+//     }
 
-    private Routine CreateRoutine()
-    {
-        return new Routine(
-            "default-user-profile-id",
-            "default-username",
-            "default-routine",
-            new TimeOnly(9, 0), // 9:00 AM
-            true,
-            "+447402033899",
-            new Preferences
-            {
-                TopicOfInterest = "default-topic",
-                ToDos = "default-todos",
-                PersonalisedPrompt = "default-prompt"
-            });
+//     private Routine CreateRoutine()
+//     {
+//         return new Routine(
+//             "default-user-profile-id",
+//             "default-username",
+//             "default-routine",
+//             new TimeOnly(9, 0), // 9:00 AM
+//             true,
+//             "+447402054824",
+//             new Preferences
+//             {
+//                 TopicOfInterest = "default-topic",
+//                 ToDos = "default-todos",
+//                 PersonalisedPrompt = "default-prompt"
+//             });
         
-    }
-}
+//     }
+// }
